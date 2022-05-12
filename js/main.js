c372122724a503a3c28165271d6aac44175a4f15
lrbn86 <lrbn86@gmail.com>
2022-05-11 23:26:16 -0400

    Update

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const popup = document.querySelector('.popup');
-const submitBtn = document.querySelector('#submit');
-
-submitBtn.addEventListener('click', (event) => {
-  popup.classList.add('submitted');
-});
